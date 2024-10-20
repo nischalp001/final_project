1e9bc7aced3dea401749f30eaca4a496122c2715
nischalpuri <2002nischal@gmail.com>
2024-10-20 17:40:54 +0545

    changes has been made in viws.py connecting to the train_moedl

--- SpamDetector/spamchecker/views.py
+++ SpamDetector/spamchecker/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .forms import EmailForm
+import joblib
 
-# Create your views here.
+# Load the trained model and vectorizer
+model = joblib.load('spamchecker/spam_model.pkl')
+vectorizer = joblib.load('spamchecker/tfidf_vectorizer.pkl')
+
+def check_spam(request):
+    result = None
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            email_text = form.cleaned_data['email_text']
+            email_vectorized = vectorizer.transform([email_text])
+            prediction = model.predict(email_vectorized)
+            result = 'Spam' if prediction[0] == 1 else 'Not Spam'
+    else:
+        form = EmailForm()
+    
+    return render(request, 'spamchecker/check_spam.html', {'form': form, 'result': result})
